7bb976ec95675a97713f04c270d2ab27802805f1
warcooft <syamsuddine9@gmail.com>
2024-09-13 22:14:31 +0700

    feat: add support for microsoft services

---
+++ src/Libraries/MicrosoftOAuth.php
@@ -0,0 +1,126 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Datamweb\ShieldOAuth\Libraries;
+
+use CodeIgniter\HTTP\CURLRequest;
+use Config\Services;
+use Datamweb\ShieldOAuth\Config\ShieldOAuthConfig;
+use Datamweb\ShieldOAuth\Libraries\Basic\AbstractOAuth;
+use Exception;
+
+class MicrosoftOAuth extends AbstractOAuth
+{
+    private static $API_CODE_URL      = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize';
+    private static $API_TOKEN_URL     = 'https://login.microsoftonline.com/common/oauth2/v2.0/token';
+    private static $API_USER_INFO_URL = 'https://graph.microsoft.com/v1.0/me';
+    private static $APPLICATION_NAME  = 'ShieldOAuth';
+
+    protected string $token;
+    protected CURLRequest $client;
+    protected ShieldOAuthConfig $config;
+    protected string $client_id;
+    protected string $client_secret;
+    protected string $callback_url;
+
+    public function __construct(string $token = '')
+    {
+        $this->token         = $token;
+        $this->client        = Services::curlrequest();
+        $this->config        = config('ShieldOAuthConfig');
+        $this->callback_url  = base_url('oauth/' . $this->config->call_back_route);
+        $this->client_id     = env('ShieldOAuthConfig.microsoft.client_id', $this->config->oauthConfigs['microsoft']['client_id']);
+        $this->client_secret = env('ShieldOAuthConfig.microsoft.client_secret', $this->config->oauthConfigs['microsoft']['client_secret']);
+    }
+
+    public function makeGoLink(string $state): string
+    {
+        $query = http_build_query([
+            'client_id'       => $this->client_id,
+            'response_type'   => 'code',
+            'redirect_uri'    => $this->callback_url,
+            'response_mode'   => 'query',
+            'approval_prompt' => 'auto',
+            'scope'           => 'User.Read',
+            'state'           => $state,
+            'sso_reload'      => true
+        ]);
+
+        return self::$API_CODE_URL . '?' . $query;
+    }
+
+    public function fetchAccessTokenWithAuthCode(array $allGet): void
+    {
+        try {
+            $response = $this->client->request('POST', self::$API_TOKEN_URL, [
+                'form_params' => [
+                    'client_id'     => $this->client_id,
+                    'client_secret' => $this->client_secret,
+                    'code'          => $allGet['code'],
+                    'redirect_uri'  => $this->callback_url,
+                    'grant_type'    => 'authorization_code',
+                ],
+                'headers' => [
+                    'User-Agent' => self::$APPLICATION_NAME . '/1.0',
+                    'Accept'     => 'application/json',
+                ],
+            ]);
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+
+        $token = json_decode($response->getBody())->access_token;
+
+        $this->setToken($token);
+    }
+
+    public function fetchUserInfoWithToken(): object
+    {
+        try {
+            $response = $this->client->request('GET', self::$API_USER_INFO_URL, [
+                'headers' => [
+                    'Accept'        => 'application/json',
+                    'User-Agent'    => self::$APPLICATION_NAME . '/1.0',
+                    'Authorization' => 'Bearer ' . $this->getToken(),
+                ],
+                'http_errors' => false,
+            ]);
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+
+        $userInfo = json_decode($response->getBody());
+
+        $userInfo->email = $userInfo->mail;
+
+        return $userInfo;
+    }
+
+    public function setColumnsName(string $nameOfProcess, object $userInfo): array
+    {
+        if ($nameOfProcess === 'syncingUserInfo') {
+            return [
+                $this->config->usersColumnsName['first_name'] => $userInfo->givenName,
+                $this->config->usersColumnsName['last_name']  => $userInfo->surname ?? null,
+                $this->config->usersColumnsName['avatar']     => null,
+            ];
+        }
+
+        if ($nameOfProcess === 'newUser') {
+            helper('text');
+
+            return [
+                'username'                                    => $userInfo->mail,
+                'email'                                       => $userInfo->mail,
+                'password'                                    => random_string('crypto', 32),
+                'active'                                      => 1, // if microsoft authentication is valid then the user account does not require an email activator
+                $this->config->usersColumnsName['first_name'] => $userInfo->givenName,
+                $this->config->usersColumnsName['last_name']  => $userInfo->surname ?? null,
+                $this->config->usersColumnsName['avatar']     => null
+            ];
+        }
+
+        return [];
+    }
+}
